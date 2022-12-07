263aacd42a03f245c764c0c7f821966567a260d5
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-12-07 22:06:20 +0900

    react compile test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Text } from "@chakra-ui/react";
 import React from "react";
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Text fontWeight={"bold"} color={"red.600"} fontSize={"6xl"}>
+        It works!
+      </Text>
+    </div>
+  );
 }
 
 export default App;
